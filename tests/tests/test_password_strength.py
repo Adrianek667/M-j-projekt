f1b722a5dbdb1c32fc762b13afe827a39fad1862
Adrianek667 <wrx98067@student.wroclaw.merito.pl>
2025-07-02 20:32:47 +0200

    Create test_password_strength.py

---
+++ tests/tests/test_password_strength.py
@@ -0,0 +1,7 @@
+from password_strength import is_strong_password
+
+def test_strong_password():
+    assert is_strong_password("Test1234!") is True
+
+def test_weak_password_no_special():
+    assert is_strong_password("Test1234") is False
